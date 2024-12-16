a1df709eda46c9701ddb1a457ddaec0c86f99632
Andrei <An.S-1@yandex.ru>
2024-12-16 14:53:58 +0300

    8

--- numbers/src/main/java/ru/example/numbers/exception/NumbersDoesNotExistException.java
+++
@@ -1,7 +0,0 @@
-package ru.example.numbers.exception;
-
-public class NumbersDoesNotExistException extends RuntimeException {
-    public NumbersDoesNotExistException(final String message) {
-        super(message);
-    }
-}
--- numbers/src/main/java/ru/example/numbers/service/numberService/NumberServiceDb.java
+++
@@ -1,7 +0,0 @@
-package ru.example.numbers.service.numberService;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.example.numbers.model.Numbers;
-
-public interface NumberServiceDb extends JpaRepository<Numbers, Long> {
-}
